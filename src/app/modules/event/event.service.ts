a77429ffbb203b23b66e08118918fb5731b2aafb
unknown <kobirul05j@gmail.com>
2025-08-30 05:03:07 +0600

    in all events add include user

--- src/app/modules/event/event.service.ts
+++ src/app/modules/event/event.service.ts
@@ -67,6 +67,7 @@ const result = await prisma.event.findMany({
         id: true,
         firstName: true,
         lastName: true,
+        email: true,
         profileImage: true
       },
     },
